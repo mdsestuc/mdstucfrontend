680b8aa088384b9ad69d5866c9ebc38a8d121c28
Ariel Medina <mdsestuc@gmail.com>
2025-01-13 17:08:47 -0300

    commit

--- src/config/DashRcAxios.js
+++ src/config/DashRcAxios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 export const dashAxios = axios.create({
     //baseURL: 'http://localhost:4000/api/',
-    baseURL: 'http://10.100.111.165:4000/api/',
+    baseURL: 'http://192.168.0.20:4000/api/',
     timeout: 12000,
 });
 
