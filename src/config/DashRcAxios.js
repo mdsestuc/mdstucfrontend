e8567079a77c362f51a57a2d26674940f5b87872
Ariel Medina <mdsestuc@gmail.com>
2025-01-14 17:23:05 -0300

    backend en vercel

--- src/config/DashRcAxios.js
+++ src/config/DashRcAxios.js
@@ -3,7 +3,8 @@ import axios from 'axios';
 
 export const dashAxios = axios.create({
     //baseURL: 'http://localhost:4000/api/',
-    baseURL: 'http://192.168.0.20:4000/api/',
+    //baseURL: 'http://192.168.0.20:4000/api/',
+    baseURL: 'https://backendmds-bhim16sob-arielmedinas-projects.vercel.app/',
     timeout: 12000,
 });
 
